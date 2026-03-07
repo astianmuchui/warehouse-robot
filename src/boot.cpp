bdd90cde4eb6d664da113c61137ac93358e4566e
astianmuchui <astianmuchui@gmail.com>
2026-03-07 18:59:42 +0300

    Added pin initialization for boot sequence

--- src/boot.cpp
+++ src/boot.cpp
@@ -1,5 +1,21 @@
+#include <Arduino.h>
 #include "defines.h"
 
-void boot_sequence() {
+
+/**
+ * @initialize_pins - Initializes the device pins
+ *
+ */
+ 
+void initialize_pins()
+{
+
+    Serial.begin(115200);
+    // Ultrasonic sensor pins
+    pinMode(HC_SR04_TRIG, OUTPUT);
+    pinMode(HC_SR04_ECHO, INPUT);
+
+    // DHT11 Pin
+    pinMode(DHT_PIN, INPUT);
 
 }
