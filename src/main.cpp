8605d8afbdeda65a5b4f0cea5d4deb5ed4c4e475
astianmuchui <astianmuchui@gmail.com>
2026-03-07 19:00:49 +0300

    Added randomized movement to test servos

--- src/main.cpp
+++ src/main.cpp
@@ -2,103 +2,140 @@
 #include <freertos/task.h>
 #include <Arduino.h>
 #include "defines.h"
-#include "FreeRTOSConfig.h"
+#include <ESP32Servo.h>
 
+Servo base, shoulder, elbow, gripper;
 
-static const BaseType_t app1_cpu = 1;
-static const BaseType_t app2_cpu = 1;
-static const BaseType_t app3_cpu = 1;
-static const BaseType_t app4_cpu = 1;
+static const BaseType_t servo_cpu = 1;
+static const BaseType_t led_cpu = 0;
 
 static const int led_pin1 = LED_BUILTIN;
 static const int led_pin2 = 32;
 static const int led_pin3 = 33;
 static const int led_pin4 = 25;
 
-
-void toggleLED1(void *parameter) {
-  while(1) {
+void toggleLED1(void *parameter)
+{
+  while (1)
+  {
     digitalWrite(led_pin1, HIGH);
     vTaskDelay(500 / portTICK_PERIOD_MS);
     digitalWrite(led_pin1, LOW);
     vTaskDelay(500 / portTICK_PERIOD_MS);
   }
+  vTaskDelete(NULL);
 }
 
-void toggleLED2(void *parameter) {
-  while(1) {
+void toggleLED2(void *parameter)
+{
+  while (1)
+  {
     digitalWrite(led_pin2, HIGH);
     vTaskDelay(400 / portTICK_PERIOD_MS);
     digitalWrite(led_pin2, LOW);
     vTaskDelay(400 / portTICK_PERIOD_MS);
   }
+  vTaskDelete(NULL);
 }
 
-void toggleLED3(void *parameter) {
-  while(1) {
+void toggleLED3(void *parameter)
+{
+  while (1)
+  {
     digitalWrite(led_pin3, HIGH);
     vTaskDelay(400 / portTICK_PERIOD_MS);
     digitalWrite(led_pin3, LOW);
     vTaskDelay(400 / portTICK_PERIOD_MS);
   }
+  vTaskDelete(NULL);
 }
 
-void toggleLED4(void *parameter) {
-  while(1) {
+void toggleLED4(void *parameter)
+{
+  while (1)
+  {
     digitalWrite(led_pin4, HIGH);
     vTaskDelay(400 / portTICK_PERIOD_MS);
     digitalWrite(led_pin4, LOW);
     vTaskDelay(400 / portTICK_PERIOD_MS);
   }
+  vTaskDelete(NULL);
 }
 
-void setup() {
+void test_servos(void *parameter)
+{
+  base.write(100);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  base.write(100);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  shoulder.write(180);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  shoulder.write(100);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  elbow.write(100);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  elbow.write(180);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  gripper.write(100);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  gripper.write(180);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  while (1)
+  {
+    base.write(rand() % 181);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+
+    shoulder.write(rand() % 181);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+
+    elbow.write(rand() % 181);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+
+    gripper.write(rand() % 181);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+  }
+
+  vTaskDelete(NULL);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  ESP32PWM::allocateTimer(4);
+
+  shoulder.attach(SHOULDER_SERVO, 500, 2400);
+  elbow.attach(ELBOW_SERVO, 500, 2400);
+  gripper.attach(GRIPPER_SERVO, 500, 2400);
+  base.attach(BASE_SERVO, 500, 2400);
+
+  shoulder.write(0);
+  elbow.write(0);
+  gripper.write(0);
+  base.write(0);
 
   pinMode(led_pin1, OUTPUT);
   pinMode(led_pin2, OUTPUT);
   pinMode(led_pin3, OUTPUT);
   pinMode(led_pin4, OUTPUT);
 
-
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              toggleLED1,    // Function to be called
-              "Toggle LED 1", // Name of task
-              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              app1_cpu);     // Run on one core for demo purposes (ESP32 only)
-
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              toggleLED2,    // Function to be called
-              "Toggle LED 2", // Name of task
-              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              app2_cpu);     // Run on one core for demo purposes (ESP32 only)
+  initialize_pins();
+  Buzz();
+  MQTT_INITIALIZE();
 
 
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              toggleLED3,    // Function to be called
-              "Toggle LED 3", // Name of task
-              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              app3_cpu);     // Run on one core for demo purposes (ESP32 only)
-
-  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
-              toggleLED4,    // Function to be called
-              "Toggle LED 4", // Name of task
-              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
-              NULL,         // Parameter to pass to function
-              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
-              NULL,         // Task handle
-              app4_cpu);     // Run on one core for demo purposes (ESP32 only)
-
+  xTaskCreatePinnedToCore(test_servos, "Test Servos", 4096, NULL, 1, NULL, servo_cpu);
+  xTaskCreatePinnedToCore(toggleLED1, "Toggle LED1", 2048, NULL, 1, NULL, led_cpu);
+  xTaskCreatePinnedToCore(toggleLED2, "Toggle LED2", 2048, NULL, 1, NULL, led_cpu);
+  xTaskCreatePinnedToCore(toggleLED3, "Toggle LED3", 2048, NULL, 1, NULL, led_cpu);
+  xTaskCreatePinnedToCore(toggleLED4, "Toggle LED4", 2048, NULL, 1, NULL, led_cpu);
 }
 
-void loop() {
-  ;
+void loop()
+{
 }
